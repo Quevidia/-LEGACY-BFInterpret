63f6f503f09d856becfc2f285488ecc7ab4e1699
Quevidia <78695093+Quevidia@users.noreply.github.com>
2021-05-03 16:05:59 +0100

    (Slight) optimisation.
    - Change type of Output in Interpret()/ASCIIToBF() from string to StringBuilder
    - Use switch statement instead of if statement for interpretation.

--- BFInterpret.cs
+++ BFInterpret.cs
@@ -2,6 +2,7 @@
 // 2021
 
 using System;
+using System.Text;
 
 namespace BFInterpret // Create the namespace, which will be defined within other scripts.
 {
@@ -35,7 +36,7 @@ namespace BFInterpret // Create the namespace, which will be defined within othe
             return new int[] { FirstNumber, SecondNumber } ; // Return the values of FirstNumber and SecondNumber in an array of int type!
         }
 
-        public static string InterpretCode(string BrainfuckCode, string UserInput = null) // Create the method responsible for interpreting all of the Brainfuck code. This will return a string consisting of the output.
+        public static StringBuilder InterpretCode(string BrainfuckCode, string UserInput = null) // Create the method responsible for interpreting all of the Brainfuck code. This will return a string consisting of the output.
         {
             if (UserInput == null) UserInput = ""; // Define an empty string for UserInput if this argument has not been specified when calling BF.InterpretCode.
 
@@ -44,7 +45,7 @@ namespace BFInterpret // Create the namespace, which will be defined within othe
             int Pointer = 0; // Create an int value which will be used as a cell pointer.
             int StringPointer = 0; // Create an int value which will be used as the pointer for each character in the brainfuck code.
             int InputPointer = 0; // Create an int value which will be used as the pointer for the user input (NOT TO BE CONFUSED WITH THE BRAINFUCK CODE).
-            string Output = ""; // Create a string value which will be the final output. This is what is going to be returned back to their respective applications.
+            StringBuilder Output = new(); // Create a string value which will be the final output. This is what is going to be returned back to their respective applications.
 
             // Create a few additional methods that will be necessary for this code. This includes the one-and-only Brainfuck interpreter!
             int VerifyPairOfBrackets(string Direction = null) // This small method will verify a pair of brackets, which will return an int value.
@@ -63,61 +64,76 @@ namespace BFInterpret // Create the namespace, which will be defined within othe
             }
             void Interpreter() // This small method will interpret the brainfuck code.
             {
-                if (BrainfuckCode[StringPointer] == '+') Cells[Pointer]++; // If the current character in the brainfuck code is +, increment the current cell that the pointer is locating.
-                else if (BrainfuckCode[StringPointer] == '-') Cells[Pointer]--; // If the current character in the brainfuck code is -, decrement the current cell that the pointer is locating.
-                else if (BrainfuckCode[StringPointer] == '>') Pointer++; // If the current character in the brainfuck code is >, incrememt the pointer's value.
-                else if (BrainfuckCode[StringPointer] == '<') Pointer--; // If the current character in the brainfuck code is <, decrement the pointer's value.
-                else if (BrainfuckCode[StringPointer] == '.') Output += Convert.ToChar(Cells[Pointer]); // If the current character in the brainfuck code is ., append the ASCII value of the current cell that the pointer is locating to the output string.
-                else if (BrainfuckCode[StringPointer] == ',' && InputPointer < UserInput.Length)  // If the current character in the brainfuck code is , and the length of the user input is larger than the value of the input pointer, the following code will run.
+                switch (BrainfuckCode[StringPointer])
                 {
-                    Cells[Pointer] = Convert.ToByte(UserInput[InputPointer]); // Set the value of the cell that the pointer is locating to the byte value of the current character in the user input.
-                    InputPointer++; // Increment the input pointer's value.
-                }
-                else if (BrainfuckCode[StringPointer] == '[') // If the current character in the brainfuck code is an opening bracket, run the following code.
-                {
-                    int ValueOfBracket = VerifyPairOfBrackets("Right"); // Place this outside of the if statement, since this will also be used to check if the pair of brackets is valid.
-                    if (Cells[Pointer] == 0) StringPointer = ValueOfBracket; // If the value of the current cell is 0, set the string pointer to the closing bracket.
-                }
-                else if (BrainfuckCode[StringPointer] == ']')
-                {
-                    int ValueOfBracket = VerifyPairOfBrackets("Left"); // Place this outside of the if statement, since this will also be used to check if the pair of brackets is valid.
-                    if (Cells[Pointer] != 0) StringPointer = ValueOfBracket; // If the value of the current cell is not 0, set the string pointer to the opening bracket.
+                    case '+': // If the current character in the brainfuck code is +, increment the current cell that the pointer is locating.
+                        Cells[Pointer]++; break;
+                    case '-': // If the current character in the brainfuck code is -, decrement the current cell that the pointer is locating.
+                        Cells[Pointer]--; break;
+                    case '>': // If the current character in the brainfuck code is >, incrememt the pointer's value.
+                        Pointer++; break;
+                    case '<': // If the current character in the brainfuck code is <, decrement the pointer's value.
+                        Pointer--; break;
+                    case '.': // If the current character in the brainfuck code is ., append the ASCII value of the current cell that the pointer is locating to the output string.
+                        Output.Append(Convert.ToChar(Cells[Pointer])); break;
+                    case ',': // If the current character in the brainfuck code is , and the length of the user input is larger than the value of the input pointer, the following code will run.
+                        if (InputPointer < UserInput.Length)
+                        {
+                            Cells[Pointer] = Convert.ToByte(UserInput[InputPointer]); // Set the value of the cell that the pointer is locating to the byte value of the current character in the user input.
+                            InputPointer++; // Increment the input pointer's value.
+                        }
+                        break;
+                    case '[': // If the current character in the brainfuck code is an opening bracket, run the following code.
+                        int Left = VerifyPairOfBrackets("Right"); // Place this outside of the if statement, since this will also be used to check if the pair of brackets is valid.
+                        if (Cells[Pointer] == 0) StringPointer = Left; // If the value of the current cell is 0, set the string pointer to the closing bracket.
+                        break;
+                    case ']': // If the current character in the brainfuck code is a closing bracket, run the following code.
+                        int Right = VerifyPairOfBrackets("Left"); // Place this outside of the if statement, since this will also be used to check if the pair of brackets is valid.
+                        if (Cells[Pointer] != 0) StringPointer = Right; // If the value of the current cell is not 0, set the string pointer to the opening bracket.
+                        break;
                 }
                 StringPointer++; // Increment the StringPointer value.
             }
 
             // Initiate the main loop. This will use a try/catch sequence to halt the loop in case any errors have been encountered during interpretations.
             try { 
-                while (StringPointer < BrainfuckCode.Length) { 
+                while (StringPointer < BrainfuckCode.Length) 
+                { 
                     Interpreter(); // Initiate the interpreter function.
                 } 
             }
-            catch (IndexOutOfRangeException) { Output = "Memory overflow. Please check your code and see if your cells have been used incorrectly."; } // If the IndexOutOfRangeException exception has been catched, set the output to tell the user that a memory overflow has occurred.
-            catch (MissingBracket) { Output = "A pair of brackets contains a missing bracket. Please check your code and see if additional brackets are requipred for your loops."; } // If the MissingBracket exception has been catched, set the output to tell the user that a pair of brackets is still open.
+            catch (IndexOutOfRangeException) 
+            { 
+                Output.Clear(); Output.Append("Memory overflow. Please check your code and see if your cells have been used incorrectly."); 
+            } // If the IndexOutOfRangeException exception has been catched, set the output to tell the user that a memory overflow has occurred.
+            catch (MissingBracket) 
+            { 
+                Output.Clear(); Output.Append("A pair of brackets contains a missing bracket. Please check your code and see if additional brackets are requipred for your loops."); 
+            } // If the MissingBracket exception has been catched, set the output to tell the user that a pair of brackets is still open.
             return Output; // Return the final output.
         }
 
-        public static string ASCIItoBF(string ASCII) // Create a method that will convert 8-bit ASCII text into Brainfuck code. This will return a string consisting of the final Brainfuck code.
+        public static StringBuilder ASCIItoBF(string ASCII) // Create a method that will convert 8-bit ASCII text into Brainfuck code. This will return a string consisting of the final Brainfuck code.
         {
             // Make all of the base values.
-            string Output = ""; // Create a variable of string type responsible for holding the final Brainfuck code.
+            StringBuilder Output = new(); // Create a variable of string type responsible for holding the final Brainfuck code.
             int DecimalValue = 0; // Create a variable of integer type that will hold the decimal value for the cell being used.
             bool FirstTryGone = false; // Create a variable of boolean type that will depict whether the first letter has been done already or not.
 
             foreach (char Char in ASCII) // Loop through every character in the ASCII string.
             {
-                if (Char > 255) return "The string that you have inputted contains non-ASCII characters."; // Check if the integer value of Char is larger than 255. If so, return a string saying that the string contains a character that is not an ASCII character.
+                if (Char > 255) return new StringBuilder("The string that you have inputted contains non-ASCII characters."); // Check if the integer value of Char is larger than 255. If so, return a string saying that the string contains a character that is not an ASCII character.
                 int Difference = Char - DecimalValue; // Set Difference to be DecimalValue subtracted from Char.
 
                 if (Math.Abs(Difference) <= 10) // Check if the absolute value of Difference is equal to or smaller than 10.
                 {
-                    if (Difference == Math.Abs(Difference)) Output = Output + new string('+', Difference); // If Difference is the same as the absolute value of Difference, concatenate Output with + times Difference.
-                    else Output = Output + new string('-', Math.Abs(Difference)); // Concatenate Output with - times the absolute value of Difference.
+                    if (Difference == Math.Abs(Difference)) Output.Append(new string('+', Difference)); // If Difference is the same as the absolute value of Difference, concatenate Output with + times Difference.
+                    else Output.Append(new string('-', Math.Abs(Difference))); // Concatenate Output with - times the absolute value of Difference.
                 }
                 else
                 {
                     int NonPrimeDecimalValue = Char; // Get the decimal value of Char and assign its value to a variable of integer type called NonPrimeDecimalValue.
-                    if (FirstTryGone) Output = Output + "[-]<"; // If FirstTryGone is true, concatenate output with [-]<. What this does is clear out the cell that is currently being pointed at, then move the pointer to the previous cell.
+                    if (FirstTryGone) Output.Append("[-]<"); // If FirstTryGone is true, concatenate output with [-]<. What this does is clear out the cell that is currently being pointed at, then move the pointer to the previous cell.
 
                     while (NonPrimeDecimalValue.IsPrime()) // Make a while loop that will loop through the code in its scope if NonPrimeDecimalValue.IsPrime() returns true.
                     {
@@ -132,10 +148,10 @@ namespace BFInterpret // Create the namespace, which will be defined within othe
                         NonPrimeDecimalValue--; CountDown++; // Decrement NonPrimeDecimalValue and then increment CountDown.
                     }
 
-                    Output = Output + $"{new string('+', NumbersToMultiply[0])}[>{new string('+', NumbersToMultiply[1])}<-]>{new string('-', CountDown)}"; // Concatenate Output with (+ times NumbersToMultiply[0])([>)(+ times NumbersToMultiply[1])(<-]>)(- times CountDown). This will increment the value of the first cell NumbersToMultiply[0] times, then create a loop that will jump to the next cell, increment its value NumbersToMultiply[1] times, go to the previous cell, decrement its value once, then end the loop. After the loop, it will jump to the next cell and then decrement its value CountDown times.
+                    Output.Append($"{new string('+', NumbersToMultiply[0])}[>{new string('+', NumbersToMultiply[1])}<-]>{new string('-', CountDown)}"); // Concatenate Output with (+ times NumbersToMultiply[0])([>)(+ times NumbersToMultiply[1])(<-]>)(- times CountDown). This will increment the value of the first cell NumbersToMultiply[0] times, then create a loop that will jump to the next cell, increment its value NumbersToMultiply[1] times, go to the previous cell, decrement its value once, then end the loop. After the loop, it will jump to the next cell and then decrement its value CountDown times.
                 }
 
-                Output = Output + "."; // Concatenate Output with .. This will be responsible for outputting the ASCII data of the value of the current cell.
+                Output.Append("."); // Concatenate Output with .. This will be responsible for outputting the ASCII data of the value of the current cell.
                 DecimalValue = Char; FirstTryGone = true; // Set DecimalValue to hold the value of Char, then set FirstTryGone to true.
             }
 
